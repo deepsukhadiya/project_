0e0154067533e2c1ef5bdcc12f99f4c521dc02e5
Deep Sukhadiya <deepsukhadiya36@gmail.com>
2024-06-26 12:11:29 +0530

    Add files via upload

---
+++ project_1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/*Q.2 Gross Salary Calculator
+Create a program that calculates the gross salary by adding percentages of HRA, DA, and TA to
+the base salary as chosen by the user.
+*/
+
+main()
+{
+	float basesalary;
+	float HRA,DA,TA,GROSSSALARY;
+	
+	printf("Enter the basesalary = ");
+	scanf("%f",&basesalary);	
+	printf("Enter the HRP = ");
+	scanf("%f",&HRA);	
+	printf("Enter the DA = ");
+	scanf("%f",&DA);	
+	printf("Enter the TA = ");
+	scanf("%f",&TA);
+	
+	HRA=(HRA/100)*basesalary;
+	DA=(DA/100)*basesalary;
+	TA=(TA/100)*basesalary;
+	
+	GROSSSALARY= basesalary+HRA+DA+TA;  
+	
+	printf("GROSSSALARY = %d", GROSSSALARY);		
+}
+
---
+++ project_3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+main()
+{
+	int angle1,angle2,angle3;
+	
+	printf("Enter the first angle = ");
+	scanf("%d",&angle1);
+    printf("Enter the first angle = ");
+	scanf("%d",&angle2);
+	
+	angle3=180+(angle10+angle2);
+	
+	printf("The third amgle is= %d",angle3);
+	
+}
